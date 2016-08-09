ae0712bbf17311d5d3b2ac61f4274864b949134d
Jesus Gonzales <gmanriqe@gmail.com>
2016-08-09 14:16:10 -0500

    16 commit
    — Añadiendo js ( para modal) en mi archivo main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-
+$(document).ready( function(){
+	$('section#pantallas a').on('click', function(){
+		console.log($(this));
+		$('#modal img').attr('src',$(this).attr('data-image-url'));
+	});
+});

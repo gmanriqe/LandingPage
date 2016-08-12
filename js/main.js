b904115c97f14caec25fa924c47f5155c92120bb
Jesus Gonzales <gmanriqe@gmail.com>
2016-08-12 13:54:33 -0500

    31 commit
    — Animando el menú con: “smothscroll.js”
    (https://github.com/cferdinandi/smooth-scroll)

--- js/main.js
+++ js/main.js
@@ -16,6 +16,15 @@ $(document).ready( function(){
 		// console.log($(this));
 		$('#modal img').attr('src',$(this).attr('data-image-url'));
 	});
+});
 
-
+// Utilizando smoth scroll
+smoothScroll.init({
+    selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)
+	selectorHeader: '[data-scroll-header]', // Selector for fixed headers (must be a valid CSS selector)
+    speed: 700, // 1s = 1000 // Integer. How fast to complete the scroll in milliseconds
+    easing: 'easeInOutQuart', // Easing pattern to use
+    offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
+    updateURL: false, // Boolean. If true, update the URL hash on scroll
+    callback: function ( anchor, toggle ) {} // Function to run after scrolling
 });

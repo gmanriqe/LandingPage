56c18154ce8cf28ab44eb94ec0d4116da53db21c
Jesus Gonzales <gmanriqe@gmail.com>
2016-08-12 13:33:41 -0500

    29 commit
    — Añadiendo clase=“normal” al <div class="navbar navbar-default
    navbar-fixed-top normal" role="navigation">
    — Haciendo uso de jQuery

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 $(document).ready( function(){
+	var nav = $('.navbar-fixed-top');
+
+	$(window).scroll(function(){
+		var scroll = $(window).scrollTop();
+		// en los 300px estuvo bien realizar el efecto
+		if(scroll >= 300) {
+			nav.removeClass('normal').addClass('efecto');
+		} else {
+			nav.removeClass('efecto').addClass('normal');
+		}
+	})
+
+
 	$('section#pantallas a').on('click', function(){
-		console.log($(this));
+		// console.log($(this));
 		$('#modal img').attr('src',$(this).attr('data-image-url'));
 	});
+
+
 });
